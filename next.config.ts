cccf83a1eba269c7c8ee99902585db0800034a12
Chirag P Patil <chiragpatil07@gmail.com>
2026-03-30 01:57:38 +0530

    chore: initial personal os architecture

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // Enables static HTML export
+  output: "export",
+  
+  // GitHub Pages default image optimization requires a server. 
+  // We disable it to allow standard <img> rendering for static exports.
+  images: {
+    unoptimized: true,
+  },
+
+  // NOTE: If your GitHub repository is named "username.github.io", leave this as is.
+  // If your repository is named "personal-os" (a project site), you must add:
+  // basePath: '/personal-os',
 };
 
 export default nextConfig;
